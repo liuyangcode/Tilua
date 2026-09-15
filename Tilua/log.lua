6128c4a7985c2092d139a7be91958e002eeca8b5
liuyangcode <pcode@sina.com>
2026-09-15 10:55:41 +0800

    feat(p1): add trace context support to structured logger

--- Tilua/log.lua
+++ Tilua/log.lua
@@ -1,6 +1,7 @@
 local string_find = string.find
 local table_concat = table.concat
 local import = require("Tilua.utils.util").import
+
 ---@class log
 local log = {
     STDERR = "STDERR",
@@ -14,9 +15,31 @@ local log = {
     DEBUG = "DEBUG",
     NONE = "NONE"
 }
+
 local mt = {
     __index = log
 }
+
+local function enrich_message(context, args)
+    if not context then
+        return args
+    end
+
+    local trace_id = nil
+    if type(context) == "table" then
+        trace_id = context.trace_id
+        if not trace_id and context.trace then
+            trace_id = context.trace.trace_id
+        end
+    end
+
+    if trace_id then
+        table.insert(args, 1, "[trace_id=" .. trace_id .. "]")
+    end
+
+    return args
+end
+
 function log:write(...)
     self:record("", ...)
 end
@@ -25,6 +48,7 @@ function log:record(level, ...)
     if level == log.NONE then
         return true
     end
+
     if self.config.type ~= 'syslog' then
         if string_find(self.config.level, level, 1, true) then
             self.log_data[#self.log_data + 1] = {
@@ -51,6 +75,12 @@ end
 
 function log:error(...)
     self:record(log.ERR,...)
+end
+
+-- P1: structured context aware logging
+function log:context(level, context, ...)
+    local args = enrich_message(context, {...})
+    self:record(level, table_concat(args, " "))
 end
 
 function log:flush()
