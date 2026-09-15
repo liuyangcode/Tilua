842cbc5930f5d8ca58914e46da7a249b17f75857
liuyangcode <pcode@sina.com>
2026-09-15 08:15:05 +0800

    feat: add service transaction helper

--- Tilua/service/service.lua
+++ Tilua/service/service.lua
@@ -9,17 +9,21 @@ function Service.new(ctx, dependencies)
 end
 
 function Service:repository(name, table_name, db_name)
-    if self.dependencies[name] then
-        return self.dependencies[name]
-    end
+    if self.dependencies[name] then return self.dependencies[name] end
     local Repository = require("Tilua.repository.repository")
     local repo = Repository.new(self.ctx, table_name, db_name)
     self.dependencies[name] = repo
     return repo
 end
 
-function Service:transaction(name)
-    return self.ctx:transaction(name)
+function Service:transaction(name, fn)
+    local tx = self.ctx:transaction(name)
+    if fn then return tx:run(fn) end
+    return tx
+end
+
+function Service:with_transaction(fn, name)
+    return self:transaction(name, fn)
 end
 
 return Service
